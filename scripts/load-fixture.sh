ddeaf518e41f44dc88bc51af3ce99b91b611c9bc
TesterRavi <testerravit@gmail.com>
2015-01-23 14:42:14 -0800

    updted script

--- scripts/load-fixture.sh
+++ scripts/load-fixture.sh
@@ -8,4 +8,3 @@ mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'hautelook'@'%';" --user=root
 mysql -e "FLUSH PRIVILEGES;" --user=root
 mysql -uroot hautelook < hautelook_dev.sql
 mysql -uroot hautelook < postd
-mysql -e 'use hautelook; select * from events WHERE event_id = 38568;'
