e8e6a2369775a92860ed748f9faa337291c05380
TesterRavi <testerravit@gmail.com>
2015-01-23 14:34:29 -0800

    updated comment

--- scripts/load-fixture.sh
+++ scripts/load-fixture.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # load-fixture
+# update permission before checking it in-- chmod ugo+x scripts/load-fixture.sh
 
 mysql -e 'create database hautelook;'
 mysql -e "create user 'hautelook'@'%';" --user=root
