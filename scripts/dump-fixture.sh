7b4b71d1e3e35f9baadd8e4344ae859dd6ba800e
TesterRavi <testerravit@gmail.com>
2015-01-29 13:57:36 -0800

    udated dump-fix. and postd script

--- scripts/dump-fixture.sh
+++ scripts/dump-fixture.sh
@@ -2,4 +2,4 @@
 # load-fixture
 # update permission before checking it in-- chmod ugo+x scripts/load-fixture.sh
 
-mysqldump -uroot -p hautelook --routines > /vagrant/sql_export/hautelook_dev.sql
+mysqldump -uroot -p hautelook --routines | gzip > /vagrant/sql_export/hautelook_dev.sql.gz
