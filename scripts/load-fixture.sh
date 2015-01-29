04cb3e4b1523932d31418a460456f58e7d041230
TesterRavi <testerravit@gmail.com>
2015-01-29 13:54:03 -0800

    added gunzip to reduce the git repo size

--- scripts/load-fixture.sh
+++ scripts/load-fixture.sh
@@ -11,6 +11,7 @@ mysql -e "create user 'hautelook'@'%';" --user=root
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'hautelook'@'%';" --user=root
 mysql -e "FLUSH PRIVILEGES;" --user=root
 mysql -e 'SET GLOBAL FOREIGN_KEY_CHECKS=1;'
+gunzip -v hautelook_dev.sql.gz
 mysql -uroot hautelook < hautelook_dev.sql
 mysql -uroot hautelook < postd
 #mysql -e 'SET GLOBAL FOREIGN_KEY_CHECKS=1;'
