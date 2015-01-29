798577d245fc9ab47d836ec0a8fcbcc8f96f5ccf
TesterRavi <testerravit@gmail.com>
2015-01-29 11:07:02 -0800

    foreign key check on

--- scripts/load-fixture.sh
+++ scripts/load-fixture.sh
@@ -6,7 +6,7 @@ mysql -e 'create database hautelook;'
 mysql -e "create user 'hautelook'@'%';" --user=root
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'hautelook'@'%';" --user=root
 mysql -e "FLUSH PRIVILEGES;" --user=root
-mysql -e 'SET GLOBAL FOREIGN_KEY_CHECKS=0;'
+mysql -e 'SET GLOBAL FOREIGN_KEY_CHECKS=1;'
 mysql -uroot hautelook < hautelook_dev.sql
 mysql -uroot hautelook < postd
-mysql -e 'SET GLOBAL FOREIGN_KEY_CHECKS=1;'
+#mysql -e 'SET GLOBAL FOREIGN_KEY_CHECKS=1;'
