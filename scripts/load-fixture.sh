42407af735683545dd167cdb5d12136cc5abf602
TesterRavi <testerravit@gmail.com>
2015-02-04 14:12:03 -0800

    split

--- scripts/load-fixture.sh
+++ scripts/load-fixture.sh
@@ -10,6 +10,7 @@ mysql -e 'create database hautelook;'
 mysql -e "create user 'hautelook'@'%';" --user=root
 mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'hautelook'@'%';" --user=root
 mysql -e "FLUSH PRIVILEGES;" --user=root
-mysql -uroot hautelook < tables/*sql
+cat hautelook* > hautelook_dev.sql
+mysql -uroot hautelook < hautelook_dev.sql
 mysql -uroot hautelook < postd
 #mysql -e 'SET GLOBAL FOREIGN_KEY_CHECKS=1;'
