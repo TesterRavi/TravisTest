f5f472c4a2a5c42ca327bc429d2169a0fcec88a4
TesterRavi <testerravit@gmail.com>
2015-02-04 16:34:39 -0800

    change seq.

--- scripts/install-fixture.sh
+++ scripts/install-fixture.sh
@@ -11,6 +11,14 @@ dbname =`mysqlshow -uroot hautelook| grep -v Wildcard | grep -o hautelook`
 set -e
 
 if [ "$dbname" == "hautelook" ]; then
+
+  cat /vagrant/sql_export/hautelook* > /vagrant/sql_export/hautelook_dev.sql
+  mysql -uroot hautelook < /vagrant/sql_export/hautelook_dev.sql
+  mysql -uroot hautelook < /vagrant/sql_export/scripts/post-deployment
+  rm /vagrant/sql_export/hautelook_dev.sql
+
+else
+
   mysql -e 'create database hautelook;'
   mysql -e "create user 'hautelook'@'%';" --user=root
   mysql -e "GRANT ALL PRIVILEGES ON * . * TO 'hautelook'@'%';" --user=root
@@ -18,12 +26,5 @@ if [ "$dbname" == "hautelook" ]; then
   cat dump-files/hautelook* > dump-files/hautelook_dev.sql
   mysql -uroot hautelook < dump-files/hautelook_dev.sql
   mysql -uroot hautelook < scripts/post-deployment
-
-else
-
-  cat /vagrant/sql_export/hautelook* > /vagrant/sql_export/hautelook_dev.sql
-  mysql -uroot hautelook < /vagrant/sql_export/hautelook_dev.sql
-  mysql -uroot hautelook < /vagrant/sql_export/scripts/post-deployment
-  rm /vagrant/sql_export/hautelook_dev.sql
 
 fi
